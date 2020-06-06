9bca948856ebb720d0a50cf456b389f9f6be5c51
Brian Bycynski <high5bri@hotmail.com>
2020-06-06 02:44:19 -0400

    finished lecture 79

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
 //
 //
 console.log(123);
+
+// BUDGET CONTROLLER
+var budgetController = (function () {
+  //
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+  //
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+
+// GLOBAL AND APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+  //
+
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function () {
+    // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+  };
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
