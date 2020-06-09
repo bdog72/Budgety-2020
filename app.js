dffa693d42470cb7b183fe07d83cc051b000a6b6
Brian Bycynski <high5bri@hotmail.com>
2020-06-09 03:11:36 -0400

    finished

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ var budgetController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
 
+  Expense.prototype.calcPercentage = function (totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
     } else {
       this.percentage = -1;
     }
